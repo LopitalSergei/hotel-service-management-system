8bcfd41804efbaec61f345bc8f7936f404b0de10
Laptev Sergei <sl260401@gmail.com>
2023-11-09 15:34:11 +0300

    add user authorization

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,9 +1,15 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/models/users/dto/create-user.dto';
 import { UsersService } from 'src/models/users/users.service';
 import * as bcrypt from 'bcryptjs';
 import { User } from 'src/models/users/entities/user.entity';
+import { log } from 'console';
 
 @Injectable()
 export class AuthService {
@@ -27,12 +33,33 @@ export class AuthService {
     });
     return this.generateToken(user);
   }
-  async login(userDto: CreateUserDto) {}
+  async login(userDto: CreateUserDto) {
+    const user = await this.validateUser(userDto);
+    return this.generateToken(user);
+  }
 
-  async generateToken(user: User) {
+  private async generateToken(user: User) {
     const payload = { email: user.email, id: user.id, roles: user.roles };
     return {
       token: this.jwtService.sign(payload),
     };
+  }
+
+  private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.findByEmail(userDto.email);
+    if (!user) {
+      throw new UnauthorizedException({
+        message: 'Неверный email!',
+      });
+    }
+    const passwordEquals = await bcrypt.compare(
+      userDto.password,
+      user.password,
+    );
+
+    if (passwordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({ message: 'Неверный пароль!' });
   }
 }
